05b5d39b44abc526ba9a7727319fbec0286bf55e
Peluchemoreno <uhhh.quigley2@gmail.com>
2022-12-11 15:55:19 -0600

    if applied this should better orgainze the readibility of my objects, and add logic for displaying a message when the size of pokemon is bigger than 1m

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,44 @@
 let pokemonList = [
-    { name: 'squirtle', height: .5, types: ['water'] },
-    { name: 'poliwrath', height: 1.3, types: ['water', 'fighting'] },
-    { name: 'charizard', height: 1.7, types: ['fire', 'flying'] },
-    { name: 'pikachu', height: .4, types: ['electric'] },
-    { name: 'umbreon', height: 1, types: ['dark'] },
-    { name: 'espeon', height: .9, types: ['psychic'] }
+    {
+        name: 'squirtle',
+        height: .5,
+        types: ['water']
+    },
+
+    {
+        name: 'poliwrath',
+        height: 1.3,
+        types: ['water', 'fighting']
+    },
+    {
+        name: 'charizard',
+        height: 1.7,
+        types: ['fire', 'flying']
+    },
+    {
+        name: 'pikachu',
+        height: .4,
+        types: ['electric']
+    },
+    {
+        name: 'umbreon',
+        height: 1,
+        types: ['dark']
+    },
+    {
+        name: 'espeon',
+        height: .9,
+        types: ['psychic']
+    }
 ];
+
+//This for loop iterates over each item in my pokemonList array
+for (i = 0; i < pokemonList.length; i++) {
+    //This if statement checks whether the height of each iteration exceedes 1m
+    //in height and displays a message if the height exceedes 1m
+    if (pokemonList[i].height > 1) {
+        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) WOW THAT'S BIG <br>`);
+    } else {
+        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) <br>`);
+    }
+}
