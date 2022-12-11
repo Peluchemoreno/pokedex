1d9e7826f2e79f57536e97a5f5c60406e12f0e30
Peluchemoreno <uhhh.quigley2@gmail.com>
2022-12-11 09:49:34 -0600

    if applied, this should add an array of pokemon objects to my js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert("Hello World")
-const favoriteFood = "cereal";
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'squirtle', height: .5, types: ['water'] },
+    { name: 'poliwrath', height: 1.3, types: ['water', 'fighting'] },
+    { name: 'charizard', height: 1.7, types: ['fire', 'flying'] },
+    { name: 'pikachu', height: .4, types: ['electric'] },
+    { name: 'umbreon', height: 1, types: ['dark'] },
+    { name: 'espeon', height: .9, types: ['psychic'] }
+];
